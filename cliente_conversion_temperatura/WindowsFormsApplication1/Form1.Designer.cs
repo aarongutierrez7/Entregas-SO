ee3d9705b46563034d27ca9fb6605b3eb069b1b8
unknown <aarongf1997@gmail.com>
2020-03-27 17:56:38 +0100

    Entrega conversor grados

---
+++ cliente_conversion_temperatura/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.temp = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.unidad = new System.Windows.Forms.TextBox();
+            this.Temperatura = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Temperatura";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // temp
+            // 
+            this.temp.Location = new System.Drawing.Point(226, 48);
+            this.temp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.temp.Name = "temp";
+            this.temp.Size = new System.Drawing.Size(81, 26);
+            this.temp.TabIndex = 3;
+            this.temp.TextChanged += new System.EventHandler(this.temp_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(38, 62);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 190);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Enviar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.unidad);
+            this.groupBox1.Controls.Add(this.Temperatura);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.temp);
+            this.groupBox1.Location = new System.Drawing.Point(34, 160);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(544, 311);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Peticion";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // unidad
+            // 
+            this.unidad.Location = new System.Drawing.Point(428, 49);
+            this.unidad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.unidad.Name = "unidad";
+            this.unidad.Size = new System.Drawing.Size(40, 26);
+            this.unidad.TabIndex = 9;
+            this.unidad.TextChanged += new System.EventHandler(this.unidad_TextChanged);
+            // 
+            // Temperatura
+            // 
+            this.Temperatura.AutoSize = true;
+            this.Temperatura.Location = new System.Drawing.Point(174, 105);
+            this.Temperatura.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Temperatura.Name = "Temperatura";
+            this.Temperatura.Size = new System.Drawing.Size(217, 24);
+            this.Temperatura.TabIndex = 8;
+            this.Temperatura.TabStop = true;
+            this.Temperatura.Text = "Dime la conversion a º o F";
+            this.Temperatura.UseVisualStyleBackColor = true;
+            this.Temperatura.CheckedChanged += new System.EventHandler(this.Temperatura_CheckedChanged_1);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(38, 511);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 82);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "desconectar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Unidad";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 865);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox temp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Temperatura;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox unidad;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
